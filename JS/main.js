0b91c927bb3691f21ea327892f70e327863ead49
JimmyPerez1 <yimianperez@gmail.com>
2025-04-25 16:15:59 -0400

    Initial Commit

--- JS/main.js
+++ JS/main.js
@@ -12,3 +12,14 @@
 
 
 /*----- functions -----*/ 
+
+
+
+
+// •	On page load: Show first question and answers
+// •	On answer click: Record selection, highlight choice
+// •	On “Next”: Advance to next question
+// •	On last question: Calculate final score and grade
+// •	On quiz end: Display grade, missed questions, replay/change options
+// •	“Play Again”: Reset state and replay
+// •	“Change Test”: Select new quiz category
